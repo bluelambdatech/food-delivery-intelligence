67b969041a2d787d4adc2ae8f5383978098beeb5
collinsorighose <collinsefe@yahoo.com>
2023-03-25 08:28:24 +0000

    added function call

--- copy_s3_file/main.py
+++ copy_s3_file/main.py
@@ -1,11 +1,10 @@
-
 import boto3
 import pandas as pd
 import os
-from dotenv import load_dotenv, find_dotenv
+##from dotenv import load_dotenv, find_dotenv
 # https://stackoverflow.com/questions/73642345/how-to-securely-pass-credentials-in-python
 
-load_dotenv(find_dotenv())  # Loads the .env file.
+#load_dotenv(find_dotenv())  # Loads the .env file.
 
 def read_s3_file(bucket_name, file_name):
     """
@@ -15,14 +14,19 @@ def read_s3_file(bucket_name, file_name):
     pandas
     python-dotenv
     """
-
     s3 = boto3.client('s3',
                       aws_access_key_id = os.getenv('ACCESS_KEY_ID'),  ## Fetch variables from the .env file.
                       aws_secret_access_key = os.getenv("SECRET_ACCESS_KEY"))## Fetch variables from the .env file.
 
     obj = s3.get_object(Bucket=bucket_name, Key=file_name)
     df = pd.read_csv(obj['Body'])
-    print (df.head())
+    #return df.head()
+    return df
 
 
-read_s3_file("uk-naija-datascience-21032023", "ny_apartment_cost_list.csv")
+
+# we can call the function as below:
+bucket_name = "uk-naija-datascience-21032023"
+file_name = "ny_apartment_cost_list.csv"
+file_contents = read_s3_file(bucket_name, file_name)
+print (file_contents)
