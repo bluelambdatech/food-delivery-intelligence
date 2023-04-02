923fe2f748588d87bb5496604ff024e51ba7f084
Blue Lambda <info@nextlambdaconsulting.com>
2023-04-02 14:46:53 -0400

    Code changes

--- test.py
+++ test.py
@@ -1,42 +1,18 @@
-import boto3
-import os
-import botocore
+import argparse
 
-s3 = boto3.client('s3',
-                  aws_access_key_id="AKIAXBP3C7WTLSEEBIP5",  ## Fetch variables from the .env file.
-                  aws_secret_access_key="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")
-
-obj = s3.get_object(Bucket="WWWWWWWWW", Key="test.csv")
-try:
-    obj = s3.get_object(Bucket="WWWWWWWWWW", Key="test.csv")
-except botocore.exceptions.ClientError:
-    print("Omolewa")
+parser = argparse.ArgumentParser(
+                    prog='ProgramName',
+                    description='What the program does',
+                    epilog='Text at the bottom of help')
 
 
+parser.add_argument('-f', '--filename')           # positional argument
+parser.add_argument('-c', '--count')      # option that takes a value
+parser.add_argument('-b', '--bucketname')
+
+args = parser.parse_args()
+print(args)
 
 
 
-x = 10
-epsilon = 0.000000000001
-y = (-50) ** (0.5)
-print(y)
-try:
-    print(var)
-except ZeroDivisionError:
-    print(x / (0 + epsilon))
-    print("was able to divide now")
-except NameError:
-    print("Joseph")
 
-
-class Nene:
-    def __init__(self):
-        pass
-
-    def __str__(self):
-        return "My name is Omolewa"
-
-
-nene = Nene()
-
-print(nene)
