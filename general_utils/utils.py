6d972a94287addd8ef14014b5076fd1ff3538489
collinsefe <collinsefe@yahoo.com>
2023-04-08 11:28:59 +0100

    code refactored

--- general_utils/utils.py
+++ general_utils/utils.py
@@ -30,26 +30,44 @@ class ReadWriteFromS3:
         self.bucketname = bucketname
         self.key = key
 
-    def read_s3_file(self, file_name, num_row=None):
+    def read_s3_file(self, key, num_row=None):
         """
         Reads a file from an S3 bucket and returns its contents as a string.
         These are the libraries required to use this function:
         :params: filename: str
         :params: num_rows: int
         """
-        # s3 = boto3.client('s3',
-        #                   aws_access_key_id=os.getenv('ACCESS_KEY_ID'),  ## Fetch variables from the .env file.
-        #                   aws_secret_access_key=os.getenv("SECRET_ACCESS_KEY"))  ## Fetch variables from the .env file.
-
-        obj = self.s3_client.get_object(Bucket=self.bucketname, Key=f"{self.key}/{file_name}")  ## Gets the file from S3
-        if file_name.split(".")[-1] in ["csv", "txt"]:
-            df = pd.read_csv(obj['Body'])  ## Reads the body of the file with pandas
-        elif file_name.split(".")[-1] == "xlsx":
-            df = pd.read_excel(obj['Body'])
+        s3 = boto3.client('s3',
+                           aws_access_key_id=os.getenv('ACCESS_KEY_ID'),  
+                           aws_secret_access_key=os.getenv("SECRET_ACCESS_KEY")) 
+        obj = self.s3_client.get_object(Bucket=self.bucketname, Key=self.key)  ## Gets the file from S3
+        buffer = io.BytesIO()
+        file_ext = key.split(".")[-1]
+        if file_ext in ["csv", "txt"]:
+            df = pd.read_csv(obj['Body'])
+            if df.shape[0] == 0:
+                exit(500)
+        elif file_ext in ["xls", "xlsx"]:
+            df = pd.read_excel(io.BytesIO(obj['Body'].read()))
+        elif file_ext  == "json":
+            num_row = None
+            df = pd.read_json(obj['Body']).to_dict()
+        elif file_ext == "parquet":
+            s3 = boto3.resource('s3',
+                        aws_access_key_id=os.getenv('ACCESS_KEY_ID'),  
+                        aws_secret_access_key=os.getenv("SECRET_ACCESS_KEY"))
+            object = s3.Object(bucket_name=bucket_name, key=key)
+            object.download_fileobj(buffer)
+            df = pd.read_parquet(buffer)
+        elif file_ext  in ["yaml", "yml"]:
+            df = yaml.safe_load(obj["Body"])
+        else:
+            print(f"{file_ext} can not be handled")
+            exit(500)
 
         if num_row:
             return df.head(num_row)
-        return df  ## End of function
+        return df   ## End of function
 
     def upload_from_local_to_s3(self, path, filename):
 
