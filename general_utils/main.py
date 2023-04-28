e2554b0db86259b4d0683a2fc2a1abbcab08fada
collinsefe <collinsefe@yahoo.com>
2023-04-28 16:05:13 +0100

    updated code

--- general_utils/main.py
+++ general_utils/main.py
@@ -2,11 +2,11 @@ from utils import *
 
 
 bucket_name = "uk-naija-datascience-21032023"
-key = "jupyter.ipynb"
+key = "hyp.scratch.yaml"
   
 readwrite = ReadWriteFromS3(aws_access_key_id=os.getenv('ACCESS_KEY_ID'),
                             aws_secret_access_key=os.getenv("SECRET_ACCESS_KEY"),
                             bucket_name=bucket_name,
                             key=key)
-df = readwrite.read_s3_file(25)
+df = readwrite.read_s3_file()
 print(df)
